e451d7272e77a30f2c66ed2a89480ed5c339bdb1
Vanshh003 <vanshaggarwal003@gmail.com>
2024-08-23 19:23:07 +0530

    Intro to JSX

--- app.js
+++ app.js
@@ -1,54 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+import { jsx } from "react/jsx-runtime";
 
 
-/* <div id = "parent">
-		<div id = "child">
-				<h1>im a h1 tag </h1>
-				<h2>im a h2 tag </h2>
-		</div>
-		<div id = "child2">
-				<h1>im a h1 tag </h1>
-				<h2>im a h2 tag </h2>
-		</div>
-</div>
-
- */
-
-
-
-
-
+// React.createElement => Object => HTML Element(render)
 
 // in react
-const parent = React.createElement(                      // takes 3 arguments.. which tag, an object, what to put inside that tag
+const heading = React.createElement(                      // takes 3 arguments.. which tag, an object, what to put inside that tag
     "div",                   // name of tag
     {id: "parent"},          // attributes
-    [
-		    React.createElement(     // child
-		        "div", 
-		        {id: "child"},
-		        [
-			        React.createElement("h1", {}, "im an h1 tag"),
-			        React.createElement("h2", {}, "im an h2 tag")
-			    ]
-		    ),
-		    React.createElement(     // child2
-		        "div", 
-		        {id: "child2"},
-		        [
-			        React.createElement("h1", {}, "im an h1 tag"),
-			        React.createElement("h2", {}, "im an h2 tag")
-			    ]
-		    )
-	  ]
+    "it's react"			 // children
 );
 
+console.log(heading);     // object
 
-console.log(parent);     // object
+
+
+
+// JSX - HTML-like or XML-like syntax
+// JSX (transocmpiled before it reaches the js engine) - PARCEL - Babel
+// JSX => React.createElement => ReactElement - JS Object => HTML Element (render)
+
+// in jsx
+const jsxHeading = <h1 id="heading">it's react using jsx</h1>
+
+console.log(jsxHeading);
+
 
 // now put this heading in/under this div id=root
 const root = ReactDOM.createRoot(document.getElementById("root"));    
 
 // render our heading inside that root
-root.render(parent);
+root.render(jsxHeading);
