5773f7d2d17b8cc57c6283787b405c8744a865ba
Vanshh003 <vanshaggarwal003@gmail.com>
2024-08-24 17:01:27 +0530

    JSX properties

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-// React Component
-const Title = function() {
-	return (
-		<h1 id="heading" className="head" tabIndex="5">
-			it's react using jsx... Title Component
-		</h1>
-	)
-};
-
-
-// Component Composition
-const HeadingComponent = () => (
-	<div id="container">
-		{/* to render Title Component into this HeadingComponent.. i.e. all that Title Component code will come here */}
-		<Title />		
-		<h1 className="heading">React Functional Component</h1>
-	</div>
+const title = (
+	<h1 className="head" tabIndex="5">
+		{elem}
+		it's react using jsx... 
+	</h1>
 );
 
 
+const TitleComponent = () => (
+	<h1 className="head" tabIndex="5">
+		Title Component
+	</h1>
+);
 
 
+// suppose we had an api which is giving us malacious data.. attacker tries to send some code when will do bad things upon execution
+const data = api.getData();
+
+const HeadingComponent = () => (
+	<div id="container">
+
+		all of the following calls/methods are the same
+		<TitleComponent />
+		<TitleComponent></TitleComponent>
+		{TitleComponent()}
+
+		{/* and that malicious js code is being executed here.. now the attacker can do an attack on you.. it can steal cookies, read local storage, read your cookies.. session storage.. get info about the system and much more..
+		this type of attack is called cross-site scripting */}
+		{data}
+		<h1 className="heading">React Functional Component</h1>
+	</div>
+);
 
  
 // now put this heading in/under this div id=root
